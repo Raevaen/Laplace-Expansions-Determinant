e9b2243d227547093198ecfca4458f1885911a6f
Raevaen <47746088+Raevaen@users.noreply.github.com>
2019-03-27 22:50:07 +0100

    Update _laplace.py

--- _laplace.py
+++ _laplace.py
@@ -1,16 +1,22 @@
-#Laplace rule to calculate determinant of a matrix bigger than 3x3
+#Laplace expansionsfor the determinant of a matrix nxn
 
 import numpy as np
 
-def _laplace(m=np.array([[0,0], [0,0]])):
-	e = np.zeros((m.shape[0], m.shape[1]))
-	for i in range(0, m.shape[0]):
-		for j in range(0, m.shape[1]):
+def _laplace(M = np.array([[0,0], [0,0]])):
+	#Initialize a void matrix with the same dimension of matrix M
+	e = np.zeros((M.shape[0], M.shape[1]))
+	#Scroll with indexes (i,j) lines and columns of the matrix
+	for i in range(0, M.shape[0]):
+		for j in range(0, M.shape[1]):
+			# p is the parameter associated to the matrix value sign 
 			p = (-1)**(i+j)
-			n = np.delete(m, i, 1)
-			n = np.delete(n, i, 0)
-	        e[i][j] = p*m[i][j]*np.linalg.det(n)   
+			# m is the reduced matrix with (n-1)x(n-1)dimensions
+			m = np.delete(M, i, 1)
+			m = np.delete(m, i, 0)
+			#Fill the matrix e
+	        e[i][j] = p*M[i][j]*np.linalg.det(m)   
 	
+	#sum(e) does the sum of all the values stored in e
 	print np.sum(e)
 
 
