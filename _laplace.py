27a4d46036d88ba05626b9ffd8cdd4ab341e803d
Raevaen <47746088+Raevaen@users.noreply.github.com>
2019-04-05 12:05:27 +0200

    Update _laplace.py

--- _laplace.py
+++ _laplace.py
@@ -2,21 +2,15 @@
 
 import numpy as np
 
-def _laplace(M = np.array([[0,0], [0,0]])):
-	#Initialize a void matrix with the same dimension of matrix M
+def _laplace(M = np.array([[0,0], [0,0]])): #Initialize a void matrix with the same dimension of matrix M
 	e = np.zeros((M.shape[0], M.shape[1]))
-	#Scroll with indexes (i,j) lines and columns of the matrix
-	for i in range(0, M.shape[0]):
+	for i in range(0, M.shape[0]): #Scroll with indexes (i,j) lines and columns of the matrix
 		for j in range(0, M.shape[1]):
-			# p is the parameter associated to the matrix value sign 
-			p = (-1)**(i+j)
-			# m is the reduced matrix with (n-1)x(n-1)dimensions
-			m = np.delete(M, i, 1)
+			p = (-1)**(i+j) # p is the parameter associated to the matrix value sign 
+			m = np.delete(M, i, 1) # m is the reduced matrix with (n-1)x(n-1)dimensions
 			m = np.delete(m, i, 0)
-			#Fill the matrix e
-	        e[i][j] = p*M[i][j]*np.linalg.det(m)   
+	        e[i][j] = p*M[i][j]*np.linalg.det(m) #Fill the matrix e   
 	
-	#sum(e) does the sum of all the values stored in e
-	print np.sum(e)
+	print np.sum(e) #sum(e) does the sum of all the values stored in e
 
 
